9b51337acfaa63f3559e21ccd7b02f0c05e2fb08
Владимир Идатиев <idatiev08@gmail.com>
2024-10-27 15:50:16 +0300

    init

---
+++ src/main/java/org/example/criptohack/CriptoHackApplication.java
@@ -0,0 +1,13 @@
+package org.example.criptohack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoHackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriptoHackApplication.class, args);
+    }
+
+}
